d5486b8f77b210088633510f26b1d6ef747623ba
Nathanael Rachmat <19623100@std.stei.itb.ac.id>
2024-09-08 00:24:26 +0700

    test: error handling and package json fix

--- table-practice/app/api/post/route.ts
+++ table-practice/app/api/post/route.ts
@@ -1,9 +1,18 @@
 import { NextResponse, NextRequest } from "next/server";
 import { db } from "@/lib/db";
+import { Prisma } from "@prisma/client";
 
 export async function GET() {
-  const table = await db.posts.findMany();
-  return NextResponse.json({ table });
+  try {
+    const table = await db.posts.findMany();
+    return NextResponse.json({ table });
+  } catch (e) {
+    if (e instanceof Prisma.PrismaClientKnownRequestError) {
+      // The .code property can be accessed in a type-safe manner
+      return NextResponse.json({ error: e.code });
+    }
+    throw e;
+  }
 }
 
 export async function POST(req: NextRequest) {
--- table-practice/app/page.tsx
+++ table-practice/app/page.tsx
@@ -1,3 +1,11 @@
 export default async function Home() {
+  await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/post`, {
+    method: "GET",
+    cache: "no-store",
+  })
+    .then(async (response) =>
+      console.log("response console:", await response.json())
+    )
+    .catch((error) => console.log("error console:", error));
   return <div className="font-bold">initial text</div>;
 }
